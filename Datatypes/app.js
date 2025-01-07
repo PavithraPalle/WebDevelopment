8bb543c1262480d12fa5357f9e2eb781599144fe
NarapuramLakshmiSwetha <2211CS010397@mallareddyuniversity.ac.in>
2025-01-07 18:19:53 +0530

    Pushing the files

---
+++ Datatypes/app.js
@@ -0,0 +1,73 @@
+console.log("Javascript is linked to html...");
+document.writeln("Javascript linked");
+
+var user = "Sam";
+user = "Miller";
+console.log(user);
+
+let num = 1000;
+num = true;
+console.log(num);
+
+const dob = 1990;
+dob = 2000;
+console.log(dob);
+
+var a = 10;
+var a = 30;
+console.log(a);
+
+// let b = 20;
+// let b = 50;
+// console.log(b); 
+// results in an error
+
+// const c = true;
+// const c = false;
+// console.log(c);
+// results in an error
+
+// function greet(name) {
+//     return `Hello, ${name}!`;
+// }
+// console.log(greet("Pavithra"));
+
+
+// let fruits = ["apple", "banana", "cherry"];
+// console.log(fruits[2]);
+
+
+// let person = {
+//     name: "Pavithra",
+//     age: 20,
+//     isStudent: true
+// };
+// console.log(person.name);
+
+
+// let a;
+// console.log(a);
+
+
+// let b = null;
+// console.log(b);
+
+
+// let bool = true;
+// console.log(bool, typeof (bool));
+
+
+// let num = 10;
+// console.log(num, typeof (num));
+
+
+// let str1 = "double quotes";
+// console.log(str1, typeof (str1));
+
+// let str2 = 'single quote';
+// console.log(str2, typeof (str2));
+
+// let str3 = `template literals`;
+// console.log(str3, typeof (str3));
+
+
