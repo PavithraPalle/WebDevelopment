a266882662e67e082003bcda047d829c790c2894
NarapuramLakshmiSwetha <2211CS010397@mallareddyuniversity.ac.in>
2025-01-08 16:01:16 +0530

    Pushing the files

---
+++ Loops/functionsexample.js
@@ -0,0 +1,19 @@
+// function demo() {
+//     console.log("Hello i am function demo()");
+// }
+// demo();
+// demo();
+
+// function add(a, b) {
+//     console.log(a + b);
+// }
+// add(5, 6);
+// add(7, 8);
+// add(3, 4);
+// add(5, 7);
+// add(10, 30);
+
+function addToCart() {
+    document.body.style.background = "teal";
+    alert("Product is added to cart successfully");
+}
---
+++ Loops/looping.js
@@ -0,0 +1,47 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log("Hello world");
+// }
+
+// let n = 100;
+// for (let i = 1; i <= n; i++) {
+//     console.log(i);
+// }
+
+// let n = 100;
+// for (let i = 1; i <= n; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i + " is an even number");
+//     } else {
+//         console.log(i + " is an odd  number");
+//     }
+// }
+
+
+// let n = prompt(Number("Enter a number: "));
+// let n = 5;
+// let factorial = 1;
+// for (let i = 1; i <= n; i++) {
+//     factorial *= i;
+// }
+// console.log(factorial);
+
+
+// let n = prompt(Number("Enter a number: "));
+// let factorial = 1;
+// for (let i = n; i >= 1; i--) {
+//     factorial *= i;
+// }
+// console.log(factorial);
+
+// let i = 1;
+// while (i <= 5) {
+//     console.log("Good Morning!");
+//     i++;
+// }
+
+let i = 1;
+do {
+    console.log("Please wake up & interact!");
+    i++;
+} while (i <= 5);
+
