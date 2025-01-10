419becd81ba9f64daccd5fbd27ec54bb3897eb1b
PavithraPalle <pallepavithra2004@gmail.com>
2025-01-10 13:38:41 +0530

    Pushing the files

---
+++ Functions-2/functions.js
@@ -0,0 +1,76 @@
+// "use strict";
+//! Higher order function
+// function hof(a) {
+//     return a();
+// };
+// let x = hof(10);
+// console.log(x);
+// let y = hof("instagram");
+// console.log(y);
+// let z = hof(true);
+// console.log(z);
+
+
+// let arr = hof([1, 2, 3, 4]);
+// console.log(arr);
+
+// let x = hof(function () { return "This is callback funciton" });
+// console.log(x);
+
+
+// function add(callback) {
+//     return callback(5, 3);
+// };
+// let x = add(function (a, b) { return a + b });
+// console.log(x);
+
+
+// let users = ["Navya", "Vishnu", "Keshav", "Shravan", "Harish"];
+// console.log(users[0]);
+// console.log(users[1]);
+
+
+// for (let i = 0; i < users.length; i++) {
+//     console.log(users[i]);
+// }
+
+
+// users.map((user) => {
+//     console.log(user);
+// });
+
+
+// let y = users.map((user) => {
+//     return user;
+// });
+// console.log(y);
+
+// let x = users.forEach((user) => {
+//     return (user);
+// });
+// console.log(x);
+
+// "use strict";
+// x = 10;
+// console.log(x);
+
+let a = 10;
+let b = 30;
+function x() {
+    var user = "Shravan";
+    let company = "Amazon";
+    const sal = 1234566;
+    console.log(user);
+    console.log(company);
+    console.log(sal);
+    console.log(a, b);
+}
+x();
+
+
+
+
+
+
+
+
