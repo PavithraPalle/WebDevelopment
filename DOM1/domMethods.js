0651705faee89b8d8493342695512aca441538ce
PavithraPalle <pallepavithra2004@gmail.com>
2025-01-24 14:15:56 +0530

    Pushing the files

---
+++ DOM1/domMethods.js
@@ -0,0 +1,112 @@
+//! document.getElementbyClassName("")
+
+// let ele = document.getElementsByClassName("test")
+// console.log(ele);
+// console.log(Array.isArray(ele));
+// console.log(typeof (ele));
+
+
+// ele[0].style.backgroundColor = "yellow";
+
+// let x = [...ele];
+// console.log(x, Array.isArray(x));
+// console.log(typeof (ele));
+
+
+// x.map((element) => {
+//     console.log(element);
+//     element.style.backgroundColor = "teal";
+
+// })
+
+// function demo() {
+//     console.log("This is a function");
+// }
+// demo();
+// let y = demo();
+// console.log(typeof (y));
+
+//! document.getElementByTagName("")
+
+// let element = document.getElementsByTagName("div");
+// console.log(element);
+
+//! document.getElementsByName("") (used in forms for input tag with name attribute)
+// let element = document.getElementsByName("username");
+// console.log(element);
+// [...element].map((ele) => {
+//     console.log(ele);
+//     ele.style.backgroundColor = "aqua";
+
+// })
+
+//! document.querySelector("# ") for id
+// let ele = document.querySelector("#demo")
+// console.log(ele);
+
+//! document.querySelector(". ") for class
+// let element = document.querySelector(".test");
+// console.log(element);
+
+//! document.querySelectorAll(". ")
+// let element = document.querySelectorAll(".test");
+// [...element].map((ele) => {
+//     console.log(ele.innerText);
+// })
+
+//! events
+//? click event
+// let btn = document.getElementById("btn");
+// btn.addEventListener("click", () => {
+//     console.log("Button clicked!");
+//     document.body.style.background = "dodgerblue";
+
+// })
+
+//? double click event
+// let btn = document.getElementById("btn");
+// btn.addEventListener("dblclick", () => {
+//     console.log("Buttton click 2 times");
+
+// })
+
+//? mouseover event
+let btn = document.getElementById("btn");
+btn.addEventListener("mouseover", () => {
+    // alert("Mouse enter");
+    document.body.style.background = "teal";
+
+})
+
+//? mouseover event
+let btn1 = document.getElementById("btn");
+btn.addEventListener("mouseleave", () => {
+    // alert("Mouse enter");
+    document.body.style.background = "tomato";
+
+})
+
+let inp = document.querySelector("#inp");
+inp.addEventListener("keydown", () => {
+    console.log("Key down");
+
+
+})
+
+inp.addEventListener("keyup", () => {
+    console.log("key up");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
